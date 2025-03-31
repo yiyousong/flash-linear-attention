c642f6b4e28e699a512ede1ee4725f83663adea8
Zhiyuan Li <uniartisan2017@gmail.com>
2025-03-31 07:06:18 +0000

    [RWKV7]: add `fused_addcmul_rwkv7` kernel

--- fla/ops/rwkv7/__init__.py
+++ fla/ops/rwkv7/__init__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from .chunk import chunk_rwkv7
+from .fused_addcmul import fused_addcmul_rwkv7
 from .fused_recurrent import fused_recurrent_rwkv7
 
 __all__ = [
     'chunk_rwkv7',
+    'fused_addcmul_rwkv7',
     'fused_recurrent_rwkv7'
 ]
