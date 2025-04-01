287ffaee26846f0370c84e6d281247c553a8986a
Y Song <56291756+yiyousong@users.noreply.github.com>
2025-04-01 10:23:43 +0800

    Update utils.py

--- fla/ops/linear_attn/utils.py
+++ fla/ops/linear_attn/utils.py
@@ -4,7 +4,9 @@ import torch
 
 
 @torch.jit.script
-def normalize_output(q, k, o):
+def normalize_output(q, k, o, cum_k=None):
     k = k.cumsum(-2)
+    if cum_k is not None:
+        k=k+cum_K
     z = (q * k).sum(-1, keepdim=True)
     return o / (z + 1e-10)
